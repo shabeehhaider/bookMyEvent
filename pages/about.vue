d69e6b5c9a997fb221790d5a12de37abecf67dac
shabeeh <shabeehhaider93@gmail.com>
2024-10-06 20:59:08 +0500

    page structure and bootstrap added

---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Welcome to your Nuxt.js App</h1>
+    <p>This is the about page!</p>
+  </div>
+</template>
+
+<script setup>
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container">
+    <h1>Welcome to your Nuxt.js App</h1>
+    <p>This is the default page!</p>
+  </div>
+</template>
+
+<script setup>
+</script>
